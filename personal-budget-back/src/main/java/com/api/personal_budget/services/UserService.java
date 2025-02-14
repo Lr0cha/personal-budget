a0da5597a57e841e3efc38a444b02abfb58b4ca9
Lucas Rocha <ldrocha16@gmail.com>
2025-02-14 20:42:05 -0300

    Criação dos metódos GET,POST e PATCH na entidade User

--- personal-budget-back/src/main/java/com/api/personal_budget/services/UserService.java
+++ personal-budget-back/src/main/java/com/api/personal_budget/services/UserService.java
@@ -4,14 +4,32 @@ import com.api.personal_budget.entities.User;
 import com.api.personal_budget.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserService {
     @Autowired
     UserRepository repository;
+
+    @Transactional(readOnly = true)
     public List<User> findAll(){
         return repository.findAll();
     }
+
+    @Transactional
+    public User insert(User user) {
+        return repository.save(user);
+    }
+
+    @Transactional
+    public User updatePassword(UUID id, String password){
+        User user = repository.findById(id).get();
+        user.setPassword(password);
+        return user;
+    }
+
 }
--- personal-budget-back/src/main/java/com/api/personal_budget/web/controllers/UserController.java
+++ personal-budget-back/src/main/java/com/api/personal_budget/web/controllers/UserController.java
@@ -3,12 +3,12 @@ package com.api.personal_budget.web.controllers;
 import com.api.personal_budget.entities.User;
 import com.api.personal_budget.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/usuarios")
@@ -20,5 +20,17 @@ public class UserController {
     public ResponseEntity<List<User>> findAll(){
         List<User> users = service.findAll();
         return ResponseEntity.ok().body(users);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        User user = service.insert(obj);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<User> updatePassword(@PathVariable UUID id, @RequestBody User obj){
+        User user = service.updatePassword(id, obj.getPassword());
+        return ResponseEntity.ok().body(user);
     }
 }

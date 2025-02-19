88a660e462530d7339444807ae6020d671030130
Lucas Rocha <ldrocha16@gmail.com>
2025-02-18 22:36:52 -0300

    Proteção de rotas

--- personal-budget-front/src/App.tsx
+++ personal-budget-front/src/App.tsx
@@ -1,13 +1,39 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./routes/PrivateRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute isPrivate={true}>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/login"
+          element={
+            <PrivateRoute isPrivate={false}>
+              <Login />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/register"
+          element={
+            <PrivateRoute isPrivate={false}>
+              <Register />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
---
+++ personal-budget-front/src/routes/PrivateRoute.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+import { Navigate } from "react-router";
+
+interface PrivateRouteProps {
+  children: ReactNode;
+  isPrivate: boolean;
+}
+
+const PrivateRoute = ({ children, isPrivate }: PrivateRouteProps) => {
+  const token = sessionStorage.getItem("token");
+
+  if (isPrivate && !token) {
+    return <Navigate to="/login" />;
+  }
+
+  if (!isPrivate && token) {
+    return <Navigate to="/" />;
+  }
+
+  return <>{children}</>;
+};
+
+export default PrivateRoute;

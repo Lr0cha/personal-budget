6ad4d019dd687bf0f6251b29095324ee884c22ec
Lucas Rocha <ldrocha16@gmail.com>
2025-02-23 19:42:16 -0300

    Configuração do banco PostgreSQL para deploy

--- personal-budget-back/src/main/java/com/api/personal_budget/config/SecurityConfig.java
+++ personal-budget-back/src/main/java/com/api/personal_budget/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/api/v1/usuarios/info").hasRole("ADMINISTRATOR")
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/usuarios").permitAll()
                         .anyRequest().authenticated())
--- personal-budget-back/src/main/java/com/api/personal_budget/entities/Expense.java
+++ personal-budget-back/src/main/java/com/api/personal_budget/entities/Expense.java
@@ -11,7 +11,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Objects;
-import java.util.UUID;
 
 @Entity
 @Getter @Setter @NoArgsConstructor

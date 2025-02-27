16af35d25b386f680ceb8e8afbf9b62037146497
Lucas Rocha <ldrocha16@gmail.com>
2025-02-27 08:52:51 -0300

    Lazy loading no lado das despesas

--- personal-budget-back/src/main/java/com/api/personal_budget/entities/Expense.java
+++ personal-budget-back/src/main/java/com/api/personal_budget/entities/Expense.java
@@ -37,7 +37,7 @@ public class Expense {
     private LocalDate createdIn;
 
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
